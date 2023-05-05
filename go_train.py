d46fca1974e3f7002262b156f2e5df00e8b1a7bf
Lynch-lam <lam570373780@gamil.com>
2023-05-05 20:35:35 +0800

    first commint

---
+++ go_train.py
@@ -0,0 +1,6 @@
+import os
+os.system("python train.py --data my_data.yaml --cfg yolov5s.yaml --weights pretrained/yolov5s.pt --epoch 1 --batch-size 1")
+
+# os.system("python train.py --data my_data.yaml --cfg yolov5s.yaml --weights pretrained/yolov5s.pt --epoch 1 --batch-size 1 --device cpu")
+# os.system("python train.py --data my_data.yaml --cfg yolov5m.yaml --weights pretrained/yolov5m.pt --epoch 100 --batch-size 4")
+# os.system("python train.py --data my_data.yaml --cfg yolov5l.yaml --weights pretrained/yolov5l.pt --epoch 100 --batch-size 4")
